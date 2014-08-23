a75b9f398d53eb49b26253cfb09b80ef5783e8a0
evanolds <ETO.creations@gmail.com>
2014-08-23 00:38:58 -0700

    Fixed version numbers for initial release

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Evan Olds (www.evanolds.com)")]
 [assembly: AssemblyProduct("Animated Cursor Maker by Evan Olds (www.evanolds.com)")]
-[assembly: AssemblyCopyright("Copyright © Evan Olds 2008")]
+[assembly: AssemblyCopyright("Copyright © Evan Olds 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
